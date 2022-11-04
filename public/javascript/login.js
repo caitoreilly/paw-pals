e26a471615dc0cce895801331cb61767cf048000
Caitlin O'Reilly <coreilly29@gmail.com>
2022-11-03 20:28:11 -0400

    created javascript folder in public folder, added login.js file to it, added code to login.handlebars file and put script to login.js at the bottom

---
+++ public/javascript/login.js
@@ -0,0 +1,23 @@
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector("#email-log").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to log in. Please try again.");
+    }
+  }
+};
+
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
