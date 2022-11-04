ca975bc06947b8f33474722504ebea00f40210cc
Brit Sovic <bsovic@tulane.edu>
2022-11-03 20:44:21 -0400

    seed, model done

--- paw-pals-seeds/post-seeds.js
+++ paw-pals-seeds/post-seeds.js
@@ -4,9 +4,23 @@ const postData = [
   {
     postTitle: 'post1',
     postDescription: 'post description 1',
+    postLocation: 'QUEENS',
+    postAvailable: 'MORNING',
+    postDog: 'name1',
+  },
+  {
+    postTitle: 'post2',
+    postDescription: 'post description 2',
     postLocation: 'burough1',
-    postAvailable: 'morning1',
-    postDog: '',
+    postAvailable: 'AFTERNOON',
+    postDog: 'name2',
+  },
+  {
+    postTitle: 'post3',
+    postDescription: 'post description 3',
+    postLocation: 'BROOKLYN',
+    postAvailable: 'EVENING',
+    postDog: 'name3',
   },
 ];
 
